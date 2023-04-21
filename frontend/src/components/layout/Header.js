5771a982dd73a7c3b64edf64cf43d70a96553536
adithyaba93@gmail.com <adithya.bendehakkalu@toyota.com>
2023-04-21 18:19:53 +0530

    added header and footer

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+import '../../App.css'
+
+const Header = () => {
+
+    return (
+        <Fragment>
+            <h1>Yen samachara</h1>
+        </Fragment>
+    )
+}
+
+export default Header
