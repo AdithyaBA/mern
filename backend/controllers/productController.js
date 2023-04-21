7a33fc1c068033e11fef659bcd8248bdd6929d67
adithyaba93@gmail.com <adithya.bendehakkalu@toyota.com>
2023-04-21 17:00:00 +0530

    installed reactjs to the frontend folder

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Errorhandler = require("../utils/errorHandle");
 
 // Create new product
 exports.newProduct = async(req, res, next) => {
@@ -23,10 +24,7 @@ exports.getAllProduct = async(req, res, next) => {
 exports.getSingleProduct = async(req, res, next) => {
     const product = await Product.findById(req.params.id);
     if(!product){
-        return res.status(400).json({
-            success: false,
-            message: "Product not found"
-        })
+        return next(new Errorhandler("Product not found", 404));
     }
     res.status(200).json({
         product
